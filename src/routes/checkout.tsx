0a3210e62ab053bb89ee7aeb270ed544266debb6
GlennBecker <logosbyglenn@gmail.com>
2022-07-15 13:11:38 -0600

    Skeleton checkout form

--- src/routes/checkout.tsx
+++ src/routes/checkout.tsx
@@ -4,6 +4,37 @@ export default component$(() => {
   return (
     <Host>
       <h2>Your Order</h2>
+      <ul>
+        TODO: Order line items go here.
+      </ul>
+      <form class="grid gap-1">
+        <fieldset>
+          <legend>Delivery Options</legend>
+          <label>
+            <input type="radio" name="delivery" />
+            <span>Pick Up</span>
+          </label>
+          <label>
+            <input type="radio" name="delivery" />
+            <span>Delivery</span>
+          </label>
+        </fieldset>
+        <label class="grid">
+          Your Name
+          <input type="text" name="name" autoComplete="full-name" />
+        </label>
+        <label class="grid">
+          Your Email
+          <input type="email" name="email" autoComplete="email" />
+        </label>
+        <label class="grid">
+          Your Phone
+          <input type="tel" name="phone" autoComplete="phone" />
+        </label>
+        <p>TODO: Show address fields if delivery option is chosen</p>
+        <p>TODO: Payment information</p>
+        <button>Checkout</button>
+      </form>
     </Host>
   );
 });
