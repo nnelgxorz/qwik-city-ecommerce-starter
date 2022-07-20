2769002006e91e599b92c5c70f4fd17380905df5
GlennBecker <logosbyglenn@gmail.com>
2022-07-20 12:28:55 -0600

    Data routes

---
+++ src/routes/api/restaurant-locations.ts
@@ -0,0 +1,27 @@
+import { EndpointHandler } from "@builder.io/qwik-city";
+import { RestaurantLocation } from "../../types";
+
+const createLocation = (name: string, id: string): RestaurantLocation => {
+  return {
+    id,
+    name,
+    address: {
+      street: `${id} Fake Street`,
+      city: name,
+    },
+  };
+};
+
+export const RESTAURANT_LOCATIONS: RestaurantLocation[] = [
+  createLocation("Partytown", "123"),
+  createLocation("Qwik City", "456"),
+  createLocation("Proxytown", "789"),
+];
+
+
+export const onGet: EndpointHandler<RestaurantLocation[]> = async () => {
+  return {
+    status: 200,
+    body: RESTAURANT_LOCATIONS
+  }
+}
---
+++ src/routes/api/restaurant-menu.ts
@@ -0,0 +1,33 @@
+import { EndpointHandler } from "@builder.io/qwik-city";
+import { MenuItem } from "../../types";
+
+export const RESTARAUNT_MENU: MenuItem[] = [
+  {
+    id: "1",
+    name: "Steve Stew-ell",
+    categories: ["Soups"],
+  },
+  {
+    id: "2",
+    name: "Bradley Beef Barley",
+    categories: ["Soups"],
+  },
+  {
+    id: "3",
+    name: "Misko Meatball Marinara",
+    categories: ["Subs"],
+  },
+  {
+    id: "4",
+    name: "Manu-roni & Cheese",
+    categories: ["Sides"],
+  },
+];
+
+
+export const onGet: EndpointHandler<MenuItem[]> = async () => {
+  return {
+    status: 200,
+    body: RESTARAUNT_MENU
+  };
+}
